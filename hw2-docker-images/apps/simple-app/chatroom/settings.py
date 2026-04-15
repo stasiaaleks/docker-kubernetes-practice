132a2b1e7722b0bab1af568e1fc1eb0ae55e0c6f
stasiaaleks <anastasiia@newtonideas.com>
2026-04-15 18:27:13 +0300

    feat: create a sample app and a dockerfile + initial ci workflow

---
+++ hw2-docker-images/apps/simple-app/chatroom/settings.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    host: str = "0.0.0.0"
+    port: int = 8080
+    max_clients: int = 20
+    server_name: str = "ChatRoom"
+
+
+settings = Settings()
